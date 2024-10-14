6bc9c191f7c4154a12fd8132f499851ddc795469
Joel <tiago7carapinha@gmail.com>
2024-10-14 18:12:00 +0100

    Fixed FOCU , fixed layout of text part in projects added more css for project section

---
+++ JS/foucFix.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.style.display = 'block'; // Change to 'visible' if using visibility
+});
+
+window.onload = () => {
+    document.body.style.visibility = 'visible';
+};
