f90a0187b1738b71252bd276ac52d1b2feeede37
Joel <tiago7carapinha@gmail.com>
2024-10-18 19:01:34 +0100

    Added button to change the content in the page

--- JS/foucFix.js
+++ JS/foucFix.js
@@ -5,3 +5,32 @@ document.addEventListener('DOMContentLoaded', () => {
 window.onload = () => {
     document.body.style.visibility = 'visible';
 };
+
+const section = ["uni", "gameJam", "misc"];
+
+function ShowHide(id)
+{
+    let el = document.getElementById(id)
+
+    if (el.style.display === 'none')
+    {
+        el.style.display = 'block'
+        section.forEach((element) => {if (element !== id) Hide(element)})
+    }
+}
+
+function Show(a)
+{
+    let el = document.getElementById(a)
+    el.style.display = 'block'
+}
+
+function Hide(a)
+{
+    let el = document.getElementById(a)
+    el.style.display = 'none'
+}
+
+//uni
+//gameJam
+//misc
